7ce9fbe30fb0d9692f6ac5ab939785cb2ef1bc96
Vinny Benson <vinny@mrv.dog>
2024-03-13 20:10:40 +0000

    hopefully, this will just add the column we need

--- workers/clips-cache/src/index.js
+++ workers/clips-cache/src/index.js
@@ -14,67 +14,84 @@ const handleAuth = async (env) => {
 const doTheThing = async (env) => {
   const DB = await env.DATABASE;
     
-    let cursor = null;
-    let err = null;
-    let clips = [];
-    let succeeded = 0;
-    let failed = 0;
+  let cursor = null;
+  let err = null;
+  let clips = [];
+  let succeeded = 0;
+  let failed = 0;
+  let removed = 0;
 
-    const auth = await handleAuth(env);
+  const auth = await handleAuth(env);
 
-    const run = async () => {
-      const url = `https://api.twitch.tv/helix/clips?game_id=${game_id}&first=100${ cursor ? `&after=${cursor}`: ''}`;
-    
-      const response = await fetch(url, {
-        headers: {
-          'Client-ID': `${env.TWITCH_CLIENT_ID}`,
-          'Authorization': `Bearer ${auth.access_token}`
-        }
-      }).catch(e => {
-        throw e;
-      });
-      const data = await response.json();
-    
-      return data;
-    }
-
-    try {
-      while(cursor !== undefined) {
-        const data = await run();
-        cursor = data.pagination.cursor;
-        clips = [...clips, ...data.data];
+  const run = async () => {
+    const url = `https://api.twitch.tv/helix/clips?game_id=${game_id}&first=100${ cursor ? `&after=${cursor}`: ''}`;
+  
+    const response = await fetch(url, {
+      headers: {
+        'Client-ID': `${env.TWITCH_CLIENT_ID}`,
+        'Authorization': `Bearer ${auth.access_token}`
       }
-      
-      const ins_stmt = DB.prepare('INSERT INTO clips (id, data) VALUES (?, ?) ON CONFLICT(id) DO UPDATE SET data = ? WHERE id = ?');
-      const ins_rows = await DB.batch([
-        ...clips.map(clip => ins_stmt.bind(
-          clip.id,
-          JSON.stringify(clip),
-          JSON.stringify(clip),
-          clip.id
-        ))
-      ]);
-
-      for(const row of ins_rows) {
-        if(row.success) {
-          succeeded++;
-        } else {
-          failed++;
-        }
-      }
-    } catch (e) {
-      err = e;
-      console.error(e);
-    }
-    
-    const error = err?.message ? { error: err } : {};
-
-    return json({
-      error,
-      failed,
-      succeeded,
+    }).catch(e => {
+      throw e;
     });
+    const data = await response.json();
+  
+    return data;
   }
+
+  try {
+    while(cursor !== undefined) {
+      const data = await run();
+      cursor = data.pagination.cursor;
+      clips = [...clips, ...data.data];
+    }
+    
+    const ins_stmt = DB.prepare('INSERT INTO clips (id, data) VALUES (?, ?) ON CONFLICT(id) DO UPDATE SET data = ? WHERE id = ?');
+    const ins_rows = await DB.batch([
+      ...clips.map(clip => ins_stmt.bind(
+        clip.id,
+        JSON.stringify(clip),
+        JSON.stringify(clip),
+        clip.id
+      ))
+    ]);
+
+    for(const row of ins_rows) {
+      if(row.success) {
+        // if updated_at is older than 7 days, remove it
+        const clip = JSON.parse(row.data);
+        const updated_at = new Date(clip.created_at).getTime();
+        const now = new Date().getTime();
+        const diff = now - updated_at;
+        const days = diff / (1000 * 60 * 60 * 24);
+
+        if(days > 7) {
+          const del_stmt = DB.prepare('DELETE FROM clips WHERE id = ?');
+          const del_row = await DB.run(del_stmt.bind(clip.id));
+          removed++;
+        } else {
+          console.log('clip is not old enough to remove', clip.id, days);
+          succeeded++;
+        }
+
+      } else {
+        failed++;
+      }
+    }
+  } catch (e) {
+    err = e;
+    console.error(e);
+  }
+  
+  const error = err?.message ? { error: err } : {};
+
+  return json({
+    ...error,
+    failed,
+    succeeded,
+    removed,
+  });
+}
 
 export default {
   async fetch(_, env) {
